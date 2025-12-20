04b2013abe8a2e9267fc4f11346876a41de75ad6
juanesalvarezruiz <juanesalvarezruiz@gmail.com>
2025-12-19 23:20:47 -0500

    Add podcast search and detail screen structure
    Create initial structure for podcast search and detail screens

---
+++ lib/features/home/presentation/screens/podcast_detail_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PodcastDetailScreen extends StatelessWidget {
+  const PodcastDetailScreen({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+  
+}
